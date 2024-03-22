a80a9d33e68cf6598c4d4b596c11d8bf9cd47c21
jessicsw <jessicaselinewong@gmail.com>
2024-03-21 20:51:51 -0700

    Initial Commit

---
+++ Duniverse/DuniverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuniverseApp.swift
+//  Duniverse
+//
+//  Created by Jessica Wong on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct DuniverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
