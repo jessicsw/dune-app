678b8730f30839731412650cbade8796e2b99030
jessicsw <jessicaselinewong@gmail.com>
2024-03-22 17:04:46 -0700

    Initial commit

---
+++ DuniverseWidget/DuniverseWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DuniverseWidgetBundle.swift
+//  DuniverseWidget
+//
+//  Created by Jessica Wong on 3/21/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DuniverseWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DuniverseWidget()
+    }
+}
