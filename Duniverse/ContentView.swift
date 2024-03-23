63bee6ce3b38b8649c75b10b74a5b3ffea0f00e7
jessicsw <jessicaselinewong@gmail.com>
2024-03-22 17:05:27 -0700

    Added API and widget extension

--- Duniverse/ContentView.swift
+++ Duniverse/ContentView.swift
@@ -7,15 +7,58 @@
 
 import SwiftUI
 
+struct Author: Codable {
+    let name: String
+}
+
+struct Book: Codable {
+    let title: String
+    let author: Author
+}
+
+struct Quote: Codable {
+    let id: String
+    let text: String
+    let book: Book
+}
+
 struct ContentView: View {
+    @State private var quote: Quote? = nil
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if let quote {
+                VStack(alignment: .leading, spacing: 10) {
+                    Text(quote.text)
+                        .font(.title)
+                    Text("Book: \(quote.book.title)")
+                    Text("Author: \(quote.book.author.name)")
+                }
+                .padding()
+            } else {
+                Text("Loading...")
+                    .task {
+                        do {
+                            quote = try await fetchRandomQuote()
+                        } catch {
+                            quote = nil
+                        }
+                    }
+            }
+            Button("Get Random Quote"){
+                Task {
+                    do {
+                        quote = try await fetchRandomQuote()
+                    } catch {
+                        quote = nil
+                    }
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.blue)
+            .controlSize(.large)
+            
         }
-        .padding()
     }
 }
 
@@ -23,4 +66,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func fetchRandomQuote() async throws -> Quote {
+    let url = URL(string: "https://api.duniverse.space/v1/random")!
+    let (data,_) = try await URLSession.shared.data(from: url)
+    let quote = try JSONDecoder().decode(Quote.self, from: data)
+    return quote
 }
--- DuniverseWidget/DuniverseWidget.swift
+++ DuniverseWidget/DuniverseWidget.swift
@@ -8,14 +8,41 @@
 import WidgetKit
 import SwiftUI
 
+struct Author: Codable {
+
+    let name: String
+
+}
+
+struct Book: Codable {
+
+    let title: String
+
+    let author: Author
+
+}
+
+struct Quote: Codable {
+
+    let id: String
+
+    let text: String
+
+    let book: Book
+
+}
+
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+        SimpleEntry(date: Date(), quote: nil)
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
-        completion(entry)
+        fetchRandomQuote { quote in
+            let entry = SimpleEntry(date: Date(), quote: quote)
+            completion(entry)
+        }
+       
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
@@ -23,27 +50,60 @@ struct Provider: TimelineProvider {
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
+        fetchRandomQuote { quote in
+            for hourOffset in 0 ..< 5 {
+                let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+                let entry = SimpleEntry(date: entryDate, quote: quote)
+                entries.append(entry)
+            }
+
+            let timeline = Timeline(entries: entries, policy: .atEnd)
+            completion(timeline)
         }
 
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
+    }
+    
+    func fetchRandomQuote(completion: @escaping (Quote?) -> Void) {
+        Task {
+            do {
+                let url = URL(string: "https://api.duniverse.space/v1/random")!
+                let (data, _) = try await URLSession.shared.data(from: url)
+                let quote = try JSONDecoder().decode(Quote.self, from: data)
+                completion(quote)
+            } catch {
+                print("Error fetching random quote: \(error)")
+                completion(nil)
+            }
+        }
     }
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
+    let quote: Quote?
 }
 
 struct DuniverseWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        Text(entry.date, style: .time)
+        if let quote = entry.quote {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(quote.text)
+                    .font(.headline)
+                Text("Book: \(quote.book.title)")
+                Text("Author: \(quote.book.author.name)")
+            }
+            .padding()
+        }
     }
+}
+
+func fetchRandomQuote() async throws -> Quote {
+    let url = URL(string: "https://api.duniverse.space/v1/random")!
+    let (data,_) = try await URLSession.shared.data(from: url)
+    let quote = try JSONDecoder().decode(Quote.self, from: data)
+    return quote
 }
 
 struct DuniverseWidget: Widget {
@@ -53,14 +113,26 @@ struct DuniverseWidget: Widget {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             DuniverseWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Duniverse Widget")
+        .description("This is a Duniverse Widget. It will show you Dune wisdom.")
     }
 }
 
 struct DuniverseWidget_Previews: PreviewProvider {
+    
     static var previews: some View {
-        DuniverseWidgetEntryView(entry: SimpleEntry(date: Date()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+        let quote = Quote(
+            id: "c59f2bd5-bcff-44cc-8cae-2b064bd7c641",
+            text: "Beyond a critical point within a finite space, freedom diminishes as numbers increase.",
+            book: Book(
+                 title: "Dune",
+                 author: Author(
+                      name: "Frank Herbert"
+                 )
+            )
+       )
+
+        DuniverseWidgetEntryView(entry: SimpleEntry(date: Date(), quote: quote))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
     }
 }
